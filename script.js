ff9e9da384d17c4ffbf9b4679c9ca6d53757053f
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-09-20 09:00:43 -0300

    versao.001

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+ const buscaEntrada = document.getElementById('busca');
+ const btnBusca = document.getElementById('btnBusca');
+ const pokemon-info = document.getElementById('pokemon-info');
 
+ async function buscaPokemon(pokemon) {
+    try{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon.tol.owerC4se()}');
+
+        if (response.ok) {
+            const data = await response.json();
+        }
+    }
+ }
